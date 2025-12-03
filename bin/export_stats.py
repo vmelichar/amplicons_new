0c8bb69d2ed3f739eb1e13bfe431c5573190e219
Vojtěch Melichar <116432329+vmelichar@users.noreply.github.com>
2025-12-03 09:22:50 +0100

    add print

--- bin/export_stats.py
+++ bin/export_stats.py
@@ -58,6 +58,8 @@ def get_basic_stats_to_dict(barcode, input):
                     'reads_short': counts[8],
                     'reads_long': counts[9],
                     'reads_filtered': counts[10]}
+        print(barcode)
+        print(dict)
         return dict
 
     else:
@@ -70,6 +72,8 @@ def get_basic_stats_to_dict(barcode, input):
                     'reads_short': 0,
                     'reads_long': 0,
                     'reads_filtered': 0}
+        print(barcode)
+        print(dict)
         return dict
 
 def get_basic_stats_to_dict_perc(dict, part=False):
