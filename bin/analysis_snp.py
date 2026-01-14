7947bb32fb3b9759419dfcc9f04345d5a0776d3e
Vojtěch Melichar <116432329+vmelichar@users.noreply.github.com>
2026-01-14 13:05:31 +0100

    correct

--- bin/analysis_snp.py
+++ bin/analysis_snp.py
@@ -97,7 +97,7 @@ def get_bases(bam, vcf_file, tbi_file, pos_file,out_dir):
             pos = int(pos)
 
             vcf_records = vcf_snps.fetch('chr' + chrom, pos-1, pos)
-            if len(vcf_records) != 1:
+            if len(list(vcf_records)) != 1:
                 print(f'VCF records for pos {chrom}:{pos} = {len(vcf_records)}')
 
             for rec in vcf_records:
